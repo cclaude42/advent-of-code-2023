90ab3ac7b5c7fd5580d272f395688196fb39dd5e
César Claude <cesarclaudepro@gmail.com>
2023-12-08 19:40:58 +0100

    cleaned up 05/p2

--- 05/p2.py
+++ 05/p2.py
@@ -1,9 +1,9 @@
 def main():
     with open("input.txt") as f:
         seeds = [int(s) for s in f.readline().split(": ")[1].split()]
-        results = []
         conversions = ["seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"]
         conversion_dict = dict()
+        results = []
 
         # For each type of conversion
         for conversion in conversions:
@@ -29,7 +29,6 @@ def main():
         # For each set of seeds...
         iter_seeds = iter(seeds)
         paired_seeds = zip(iter_seeds, iter_seeds)
-        lowest = []
         for start, length in paired_seeds:
             seedranges = [range(start, start+length)]
 
@@ -39,11 +38,9 @@ def main():
 
                 # Iterate through a subrange
                 for seedrange in seedranges:
-                    if not seedrange:
-                        continue
-
                     for rng in conversion_dict[conversion]:
                         conversion_range = range(rng["src"], rng["src"] + rng["length"])
+
                         # If ranges intersect...
                         if seedrange[0] <= conversion_range[-1] and conversion_range[0] <= seedrange[-1]:
                             r = range(max(seedrange[0], conversion_range[0]), min(seedrange[-1], conversion_range[-1]) + 1)
@@ -62,16 +59,15 @@ def main():
                             seedrange = None
                             break
 
-                    # If no match
+                    # If no match, leave range as it is
                     if seedrange:
                         converted_seedranges.append(seedrange)
 
                 seedranges = converted_seedranges
 
-            lowest.append(min([min(r) for r in seedranges]))
+            results += [min(r) for r in seedranges]
 
-        print(min(lowest))
-
+        print(min(results))
 
 
 if __name__ == "__main__":
